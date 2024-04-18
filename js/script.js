f964a005f45867627a5f4552ba0fbcbee32fc48c
manoharmohanta <manoharmohanta@rediffmail.com>
2024-04-18 21:12:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,6 +36,50 @@ document.addEventListener("DOMContentLoaded", function() {
         setActiveLink();
     });
 
-    // Load footer content
-    loadContent("pages/footer.html", "footerContainer");
+   // Load footer content
+    loadContent("pages/footer.html", "footerContainer", function(){
+        // WhatsApp icon click event
+        document.getElementById('whatsapp-icon').addEventListener('click', function() {
+            var iconSrc = document.getElementById('whatsapp-img').getAttribute('src');
+            if (iconSrc === '../images/WhatsApp_icon.png') { // Make sure the iconSrc matches the initial image source
+                openWhatsAppForm();
+            } else {
+                closeWhatsAppForm();
+            }
+        });
+
+        // Function to open WhatsApp form
+        function openWhatsAppForm() {
+            document.getElementById('whatsapp-form-container').style.display = 'block';
+            document.getElementById('whatsapp-img').setAttribute('src', '../images/close_icon.png'); // Correct the path to the close icon image
+        }
+
+        // Function to close WhatsApp form
+        function closeWhatsAppForm() {
+            document.getElementById('whatsapp-form-container').style.display = 'none';
+            document.getElementById('whatsapp-img').setAttribute('src', '../images/WhatsApp_icon.png'); // Correct the path to the WhatsApp icon image
+        }
+
+        // WhatsApp form submission event
+        document.getElementById('whatsapp-form').addEventListener('submit', function(e) {
+            e.preventDefault();
+            var name = document.getElementById('name').value;
+            var contact = document.getElementById('contact').value;
+            var email = document.getElementById('email').value;
+
+            var message = 'Name: ' + name + '%0AContact: ' + contact + '%0AEmail: ' + email;
+
+            var whatsappUrl = 'https://wa.me/+917093332363?text=' + message;
+
+            window.open(whatsappUrl, '_blank');
+
+            // Clear form fields
+            document.getElementById('name').value = '';
+            document.getElementById('contact').value = '';
+            document.getElementById('email').value = '';
+
+            // Close WhatsApp form
+            closeWhatsAppForm();
+        });
+    });
 });
